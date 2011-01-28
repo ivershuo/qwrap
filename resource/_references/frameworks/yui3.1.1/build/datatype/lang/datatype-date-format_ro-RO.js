31355b8c7f18934cb6684013ba32071318710e89
akira-cn <akira.cn@gmail.com>
2011-01-28 18:26:07 +0800

    gbk to utf-8

--- resource/_references/frameworks/yui3.1.1/build/datatype/lang/datatype-date-format_ro-RO.js
+++ resource/_references/frameworks/yui3.1.1/build/datatype/lang/datatype-date-format_ro-RO.js
@@ -5,4 +5,4 @@ http://developer.yahoo.com/yui/license.html
 version: 3.1.1
 build: 47
 */
-YUI.add("lang/datatype-date-format_ro-RO",function(A){A.Intl.add("datatype-date-format","ro-RO",{"a":["Du","Lu","Ma","Mi","Jo","Vi","Sa"],"A":["duminic?","luni","mar?i","miercuri","joi","vineri","samb?t?"],"b":["ian.","feb.","mar.","apr.","mai","iun.","iul.","aug.","sept.","oct.","nov.","dec."],"B":["ianuarie","februarie","martie","aprilie","mai","iunie","iulie","august","septembrie","octombrie","noiembrie","decembrie"],"c":"%a, %d %b %Y, %H:%M:%S %Z","p":["AM","PM"],"P":["am","pm"],"x":"%d.%m.%Y","X":"%H:%M:%S"});},"3.1.1");
+YUI.add("lang/datatype-date-format_ro-RO",function(A){A.Intl.add("datatype-date-format","ro-RO",{"a":["Du","Lu","Ma","Mi","Jo","Vi","Sâ"],"A":["duminică","luni","marți","miercuri","joi","vineri","sâmbătă"],"b":["ian.","feb.","mar.","apr.","mai","iun.","iul.","aug.","sept.","oct.","nov.","dec."],"B":["ianuarie","februarie","martie","aprilie","mai","iunie","iulie","august","septembrie","octombrie","noiembrie","decembrie"],"c":"%a, %d %b %Y, %H:%M:%S %Z","p":["AM","PM"],"P":["am","pm"],"x":"%d.%m.%Y","X":"%H:%M:%S"});},"3.1.1");

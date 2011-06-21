67c2a3666a901146922fd7d1fa307b598b8740d6
jkisjk <jk_10000@yahoo.com.cn>
2011-06-21 22:34:33 +0800

    修复NodeH的opacity IE9bug。

--- resource/js/core/custevent.js
+++ resource/js/core/custevent.js
@@ -76,10 +76,6 @@
 		 * @throw {Error} 如果没有对事件进行初始化，则会抛错
 		 */
 		on: function(target, sEvent, fn) {
-			if(!fn) return;
-			if("string" == typeof fn){
-				fn = new Function(fn);
-			}
 			var cbs = (target.__custListeners && target.__custListeners[sEvent]) || QW.error("unknown event type", TypeError);
 			if (indexOf(cbs, fn) > -1) {
 				return false;

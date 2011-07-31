d0c189fe627b8cc526ed72d1295e55de3a77a9e4
jkisjk <jk_10000@yahoo.com.cn>
2011-07-31 16:02:59 +0800

    修复ie6升级到ie8时的useragent判断失误，以及为某些移动设设备作判断

--- resource/js/core/browser.js
+++ resource/js/core/browser.js
@@ -6,20 +6,22 @@
 
 
 /**
- * @class Browser js的运行环境，浏览器以及版本信息。（Browser仅基于userAgent进行嗅探，存在不严谨的缺陷。）
+ * @class Browser js的运行环境，浏览器以及版本信息。（Browser仅基于userAgent进行嗅探，存在不严谨的缺陷。）移动的useragent信息参考自http://mo.wed.ivershuo.com/。
  * @singleton 
  * @namespace QW 
  */
 QW.Browser = (function() {
 	var na = window.navigator,
 		ua = na.userAgent.toLowerCase(),
-		browserTester = /(msie|webkit|gecko|presto|opera|safari|firefox|chrome|maxthon)[ \/]([\d.]+)/ig,
+		browserTester = /(msie|webkit|gecko|presto|opera|safari|firefox|chrome|maxthon|android|ipad|iphone|webos|hpwos)[ \/os]*([\d_.]+)/ig,
 		Browser = {
 			platform: na.platform
 		};
 	ua.replace(browserTester, function(a, b, c) {
 		var bLower = b.toLowerCase();
-		Browser[bLower] = c;
+		if (!Browser[bLower]) {
+			Browser[bLower] = c;
+		}
 	});
 	if (Browser.opera) { //Opera9.8后版本号位置变化
 		ua.replace(/opera.*version\/([\d.]+)/, function(a, b) {

92144e6d3c15f595c0e8e6335dcff9b5f3fc435a
jkisjk <jk_10000@yahoo.com.cn>
2011-10-23 17:53:38 +0800

    change tmpl方法

--- resource/js/core/string.h.js
+++ resource/js/core/string.h.js
@@ -58,55 +58,176 @@
 			});
 		},
 
-		/**
-		 * 微型模板引擎 tmpl 0.2 
-		 * http://www.planeart.cn/?p=1594
-		 * 0.2 更新:
-		 * 1. 修复转义字符与id判断的BUG
-		 * 2. 放弃低效的 with 语句从而最高提升3.5倍的执行效率
-		 * 3. 使用随机内部变量防止与模板变量产生冲突
-		 * 4. 支持 if(...): ... endif，for和while类似
-		 * @see		http://ejohn.org/blog/javascript-micro-templating/
-		 * @param	{String}	模板内容或者装有模板内容的元素ID
-		 * @param	{Object}	附加的数据
-		 * @return	{String}	解析好的模板
-		 * @example
-			var text = "{%=x+y%}";
-
-			StringH.tmpl(text, {x:"hello ", y:"world!"});
+		/** 
+		 * 字符串模板
+		 * @method tmpl
+		 * @static
+		 * @param {String} sTmpl 字符串模板，其中变量以{$aaa}表示。模板语法：
+		 分隔符为{xxx}，"}"之前没有空格字符。
+		 js表达式/js语句里的'}', 需使用' }'，即前面有空格字符
+		 {strip}...{/strip}里的所有\r\n打头的空白都会被清除掉
+		 {}里只能使用表达式，不能使用语句，除非使用以下标签
+		 {js ...}		－－任意js语句, 里面如果需要输出到模板，用print("aaa");
+		 {if(...)}		－－if语句，写法为{if($a>1)},需要自带括号
+		 {elseif(...)}	－－elseif语句，写法为{elseif($a>1)},需要自带括号
+		 {else}			－－else语句，写法为{else}
+		 {/if}			－－endif语句，写法为{/if}
+		 {for(...)}		－－for语句，写法为{for(var i=0;i<1;i++)}，需要自带括号
+		 {/for}			－－endfor语句，写法为{/for}
+		 {while(...)}	－－while语句,写法为{while(i-->0)},需要自带括号
+		 {/while}		－－endwhile语句, 写法为{/while}
+		 * @param {Object} opts (Optional) 模板参数
+		 * @return {String|Function}  如果调用时传了opts参数，则返回字符串；如果没传，则返回一个function（相当于把sTmpl转化成一个函数）
+		 
+		 * @example alert(tmpl("{$a} love {$b}.",{a:"I",b:"you"}));
+		 * @example alert(tmpl("{js print('I')} love {$b}.",{b:"you"}));
 		 */
-		tmpl: (function ($) {
-			return function (str, data) {
-				var fn = function (data) {
-							var i, variable = [$], value = [[]];
-							for (i in data) {
-								variable.push(i);
-								value.push(data[i]);
-							};
+		tmpl: (function() {
+			/*
+			sArrName 拼接字符串的变量名。
+			*/
+			var sArrName = "sArrCMX",
+				sLeft = sArrName + '.push("';
+			/*
+				tag:模板标签,各属性含义：
+				tagG: tag系列
+				isBgn: 是开始类型的标签
+				isEnd: 是结束类型的标签
+				cond: 标签条件
+				rlt: 标签结果
+				sBgn: 开始字符串
+				sEnd: 结束字符串
+			*/
+			var tags = {
+				'js': {
+					tagG: 'js',
+					isBgn: 1,
+					isEnd: 1,
+					sBgn: '");',
+					sEnd: ';' + sLeft
+				},
+				//任意js语句, 里面如果需要输出到模板，用print("aaa");
+				'if': {
+					tagG: 'if',
+					isBgn: 1,
+					rlt: 1,
+					sBgn: '");if',
+					sEnd: '{' + sLeft
+				},
+				//if语句，写法为{if($a>1)},需要自带括号
+				'elseif': {
+					tagG: 'if',
+					cond: 1,
+					rlt: 1,
+					sBgn: '");} else if',
+					sEnd: '{' + sLeft
+				},
+				//if语句，写法为{elseif($a>1)},需要自带括号
+				'else': {
+					tagG: 'if',
+					cond: 1,
+					rlt: 2,
+					sEnd: '");}else{' + sLeft
+				},
+				//else语句，写法为{else}
+				'/if': {
+					tagG: 'if',
+					isEnd: 1,
+					sEnd: '");}' + sLeft
+				},
+				//endif语句，写法为{/if}
+				'for': {
+					tagG: 'for',
+					isBgn: 1,
+					rlt: 1,
+					sBgn: '");for',
+					sEnd: '{' + sLeft
+				},
+				//for语句，写法为{for(var i=0;i<1;i++)},需要自带括号
+				'/for': {
+					tagG: 'for',
+					isEnd: 1,
+					sEnd: '");}' + sLeft
+				},
+				//endfor语句，写法为{/for}
+				'while': {
+					tagG: 'while',
+					isBgn: 1,
+					rlt: 1,
+					sBgn: '");while',
+					sEnd: '{' + sLeft
+				},
+				//while语句,写法为{while(i-->0)},需要自带括号
+				'/while': {
+					tagG: 'while',
+					isEnd: 1,
+					sEnd: '");}' + sLeft
+				} //endwhile语句, 写法为{/while}
+			};
 
-							return (new Function(variable, fn.$))
-							.apply(data, value).join("");
-						};
-				
-				fn.$ = fn.$ || "var print=function(){" + $ + ".push.apply(" + $ + ",arguments)};"
-				+ $ + ".push('" 
-				+ str.replace(/\\/g, "\\\\")
-					 .replace(/[\r\t\n]/g, " ")
-					 .replace(/({%\s*)elsif/g,"$1else if")
-					 .replace(/({%\s*)(else\s*(if.*?)?:)/g,"$1} $2") //else:, else if...: 要补全前面的 }
-					 .replace(/:\s*(%})/g,"{ $1") //no label, support {% if(...): %}
-					 .replace(/end(?:if|for|while)\s*(%})/g,"} $1")
-					 .split("{%").join("\t")
-					 .replace(/((^|%})[^\t]*)'/g, "$1\r")
-					 .replace(/\t=(.*?)%}/g, "',$1,'")
-					 .split("\t").join("');")
-					 .split("%}").join(";" + $ + ".push('")
-					 .split("\r").join("\\'")
-				+ "');return " + $;
-
-				return data ? fn(data) : fn;
-			}
-		})('$' + (+new Date)),
+			return function(sTmpl, opts) {
+				var N = -1,
+					NStat = []; //语句堆栈;
+				var ss = [
+					[/\{strip\}([\s\S]*?)\{\/strip\}/g, function(a, b) {
+						return b.replace(/[\r\n]\s*\}/g, " }").replace(/[\r\n]\s*/g, "");
+					}],
+					[/\\/g, '\\\\'],
+					[/"/g, '\\"'],
+					[/\r/g, '\\r'],
+					[/\n/g, '\\n'], //为js作转码.
+					[
+						/\{[\s\S]*?\S\}/g, //js里使用}时，前面要加空格。
+						function(a) {
+							a = a.substr(1, a.length - 2);
+							for (var i = 0; i < ss2.length; i++) {a = a.replace(ss2[i][0], ss2[i][1]); }
+							var tagName = a;
+							if (/^(.\w+)\W/.test(tagName)) {tagName = RegExp.$1; }
+							var tag = tags[tagName];
+							if (tag) {
+								if (tag.isBgn) {
+									var stat = NStat[++N] = {
+										tagG: tag.tagG,
+										rlt: tag.rlt
+									};
+								}
+								if (tag.isEnd) {
+									if (N < 0) {throw new Error("Unexpected Tag: " + a); }
+									stat = NStat[N--];
+									if (stat.tagG != tag.tagG) {throw new Error("Unmatch Tags: " + stat.tagG + "--" + tagName); }
+								} else if (!tag.isBgn) {
+									if (N < 0) {throw new Error("Unexpected Tag:" + a); }
+									stat = NStat[N];
+									if (stat.tagG != tag.tagG) {throw new Error("Unmatch Tags: " + stat.tagG + "--" + tagName); }
+									if (tag.cond && !(tag.cond & stat.rlt)) {throw new Error("Unexpected Tag: " + tagName); }
+									stat.rlt = tag.rlt;
+								}
+								return (tag.sBgn || '') + a.substr(tagName.length) + (tag.sEnd || '');
+							} else {
+								return '",(' + a + '),"';
+							}
+						}
+					]
+				];
+				var ss2 = [
+					[/\\n/g, '\n'],
+					[/\\r/g, '\r'],
+					[/\\"/g, '"'],
+					[/\\\\/g, '\\'],
+					[/\$(\w+)/g, 'opts["$1"]'],
+					[/print\(/g, sArrName + '.push(']
+				];
+				for (var i = 0; i < ss.length; i++) {
+					sTmpl = sTmpl.replace(ss[i][0], ss[i][1]);
+				}
+				if (N >= 0) {throw new Error("Lose end Tag: " + NStat[N].tagG); }
+				sTmpl = 'var ' + sArrName + '=[];' + sLeft + sTmpl + '");return ' + sArrName + '.join("");';
+				//alert('转化结果\n'+sTmpl);
+				var fun = new Function('opts', sTmpl);
+				if (arguments.length > 1) {return fun(opts); }
+				return fun;
+			};
+		}()),
 
 		/** 
 		 * 判断一个字符串是否包含另一个字符串

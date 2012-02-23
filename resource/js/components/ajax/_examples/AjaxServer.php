11933f5482c48bbb4371f4f28ff44492eaa99947
Jerry Qu <quguangyu@gmail.com>
2012-02-24 02:27:40 +0800

    up
    Signed-off-by: Jerry Qu <quguangyu@gmail.com>

--- resource/js/components/ajax/_examples/AjaxServer.php
+++ resource/js/components/ajax/_examples/AjaxServer.php
@@ -3,4 +3,4 @@
 @header('Cache-Control: no-cache'); @header('Pragma: no-cache');
 $_d = $_GET["delay"]/1000;
 sleep($_d);
-?>{err:'<?php if($_GET["err"] == "mcphp.fatal") print("mcphp.fatal"); else print("mcphp.ok"); ?>',city:"shanghai",user:"JK_QW"}
+?>{err:'<?php if($_POST["err"] == "mcphp.fatal") print("mcphp.fatal"); else print("mcphp.ok"); ?>',city:"shanghai",user:"JK_QW"}

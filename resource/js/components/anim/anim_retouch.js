3fbbc53408476ae28884ae79bfe3a2a5e3faa305
Jerry Qu <quguangyu@gmail.com>
2012-02-23 22:01:52 +0800

    qwrap1.1.0
    Signed-off-by: Jerry Qu <quguangyu@gmail.com>

---
+++ resource/js/components/anim/anim_retouch.js
@@ -0,0 +1,182 @@
+/*
+ *	Copyright (c) QWrap
+ *	version: $version$ $release$ released
+ *	author:Jerry(屈光宇)、JK（加宽）
+ *  description: Anim推荐retouch....
+*/
+
+(function() {
+	var NodeH = QW.NodeH,
+		g = NodeH.g,
+		show = NodeH.show,
+		hide = NodeH.hide,
+		isVisible = NodeH.isVisible,
+		getStyle = NodeH.getCurrentStyle,
+		getSize = NodeH.getSize,
+		setStyle = NodeH.setStyle;
+
+	function newAnim(el, attrs, callback, dur, easing) {
+		el = g(el);
+		var preAnim = el.__preAnim;
+		preAnim && preAnim.isPlaying() && preAnim.pause();
+
+		var anim = new QW.ElAnim(el, attrs, dur || 400, easing);
+		if (callback) {
+			anim.on("end", function() {
+				callback();
+			});
+		}
+		anim.play();
+		el.__preAnim = anim;
+		return anim;
+	}
+
+	var AnimElH = {
+		/**
+		 * [animate 通用的动画函数]
+		 * @param  {[Element]}   el    动画作用的元素
+		 * @param  {[Object]}   attrs  动画改变的属性
+		 * @param  {[Int]}   dur       动画时长，毫秒
+		 * @param  {Function} callback 动画运行完回调
+		 * @param  {Function}   easing 动画算子
+		 * @return ElAnim
+		 */
+		animate: function(el, attrs, dur, callback, easing) {
+			return newAnim(el, attrs, callback, dur, easing);
+		},
+
+		/**
+		 * [fadeIn 淡入]
+		 * @param  {[Element]}   el    动画作用的元素
+		 * @param  {[Int]}   dur       动画时长，毫秒
+		 * @param  {Function} callback 动画运行完回调
+		 * @param  {Function}   easing 动画算子
+		 * @return ElAnim
+		 */
+		fadeIn: function(el, dur, callback, easing) {
+			show(el);
+			return newAnim(el, {
+				"opacity": {
+					to: el.__animOpacity || 1
+				}
+			}, callback, dur, easing);
+		},
+
+		/**
+		 * [fadeOut 淡出]
+		 * @param  {[Element]}   el    动画作用的元素
+		 * @param  {[Int]}   dur       动画时长，毫秒
+		 * @param  {Function} callback 动画运行完回调
+		 * @param  {Function}   easing 动画算子
+		 * @return ElAnim
+		 */
+		fadeOut: function(el, dur, callback, easing) {
+			callback = callback || function() {
+				hide(el);
+			};
+			el.__animOpacity = el.__animOpacity || getStyle(el, 'opacity');
+			return newAnim(el, {
+				"opacity": {
+					to: 0
+				}
+			}, callback, dur, easing);
+		},
+
+		/**
+		 * [fadeToggle 淡入/淡出切换]
+		 * @param  {[Element]}   el    动画作用的元素
+		 * @param  {[Int]}   dur       动画时长，毫秒
+		 * @param  {Function} callback 动画运行完回调
+		 * @param  {Function}   easing 动画算子
+		 * @return ElAnim
+		 */
+		fadeToggle: function(el, dur, callback, easing) {
+			return AnimElH[isVisible(el) ? 'fadeOut' : 'fadeIn'](el, dur, callback, easing);
+		},
+
+		/**
+		 * [slideUp 收起]
+		 * @param  {[Element]}   el    动画作用的元素
+		 * @param  {[Int]}   dur       动画时长，毫秒
+		 * @param  {Function} callback 动画运行完回调
+		 * @param  {Function}   easing 动画算子
+		 * @return ElAnim
+		 */
+		slideUp: function(el, dur, callback, easing) {
+			var from = getSize(el).height;
+
+			callback = callback || function() {
+				hide(el);
+			};
+
+			el.__animHeight = el.__animHeight || from;
+
+			return newAnim(el, {
+				"height": {
+					from :from,
+					to: 0
+				}
+			}, callback, dur, easing);
+		},
+
+		/**
+		 * [slideDown 展开]
+		 * @param  {[Element]}   el    动画作用的元素
+		 * @param  {[Int]}   dur       动画时长，毫秒
+		 * @param  {Function} callback 动画运行完回调
+		 * @param  {Function}   easing 动画算子
+		 * @return ElAnim
+		 */
+		slideDown: function(el, dur, callback, easing) {
+			var from = 0;
+
+			if(!isVisible(el)) {
+				show(el);
+			} else {
+				from = getSize(el).height;
+			}
+
+			return newAnim(el, {
+				"height": {
+					from:from,
+					to: el.__animHeight || getSize(el).height
+				}
+			}, callback, dur, easing);
+		},
+
+		/**
+		 * [slideToggle 收起/展开切换]
+		 * @param  {[Element]}   el    动画作用的元素
+		 * @param  {[Int]}   dur       动画时长，毫秒
+		 * @param  {Function} callback 动画运行完回调
+		 * @param  {Function}   easing 动画算子
+		 * @return ElAnim
+		 */
+		slideToggle: function(el, dur, callback, easing) {
+			return AnimElH[isVisible(el) ? 'slideUp' : 'slideDown'](el, dur, callback, easing);
+		},
+
+		/**
+		 * [shine4Error 颜色渐变，用于表单提示]
+		 * @param  {[Element]}   el    动画作用的元素
+		 * @param  {[Int]}   dur       动画时长，毫秒
+		 * @param  {Function} callback 动画运行完回调
+		 * @param  {Function}   easing 动画算子
+		 * @return ElAnim
+		 */
+		shine4Error: function(el, dur, callback, easing) {
+			return newAnim(el, {
+				"backgroundColor": {
+					from: "#f33",
+					to: "#fff",
+					end: ""
+				}
+			}, callback, dur, easing);
+		}
+	};
+
+	QW.NodeW.pluginHelper(AnimElH, 'operator');
+	if (QW.Dom) {
+		QW.ObjectH.mix(QW.Dom, AnimElH);
+	}
+}());
---
+++ resource/js/components/anim/elanim.js
@@ -0,0 +1,229 @@
+/*
+ * @fileoverview Anim
+ * @author:Jerry Qu、JK、Akira_cn
+ */
+
+(function() {
+	var NodeH = QW.NodeH,
+		mix = QW.ObjectH.mix,
+		mixMentor = mix, //顾问模式
+		CustEvent = QW.CustEvent,
+		g = NodeH.g,
+		getCurrentStyle = NodeH.getCurrentStyle,
+		setStyle = NodeH.setStyle,
+		isElement = QW.DomU.isElement,
+		map = QW.ArrayH.map,
+		Anim = QW.Anim;
+
+
+	var ElAnimAgent = function(el, opts, attr) {
+		this.el = el;
+		this.attr = attr;
+
+		mix(this, opts);
+	};
+
+	mix(ElAnimAgent.prototype, {
+		getValue : function(){
+			return getCurrentStyle(this.el, this.attr);
+		},
+		setValue : function(value, unit){
+			setStyle(this.el, this.attr, value + unit);
+		},
+		getUnit : function() {
+			if(this.unit) return this.unit;
+			
+			var value = this.getValue(), 
+				cssNumber = ["zIndex", "fontWeight", "opacity", "lineHeight"];
+
+			if(value) {
+				var unit = value.toString().replace(/^[+-]?[\d\.]+/g, '');
+				if(unit && unit != value) {
+					return unit;
+				}
+			}
+
+			if( !cssNumber.contains(this.attr.camelize()) ) {
+				return 'px';
+			}
+
+			return '';
+		},
+		init : function() {
+			var from, to, by, unit;
+			if(null != this.from){
+				from = parseFloat(this.from);			
+			}else{
+				from = parseFloat(this.getValue()) || 0;
+			}
+
+			to = parseFloat(this.to);
+			by = this.by != null ? parseFloat(this.by) : (to - from);	
+
+			this.from = from;
+			this.by = by;
+			this.unit = this.getUnit();
+		},
+		action : function(per){
+			var unit = this.unit, value;
+			if (typeof this.end != "undefined" && per >= 1) {
+				value = this.end;
+			} else {
+				value = this.from + this.by * this.easing(per);
+				value = value.toFixed(6);
+			}
+			this.setValue(value, unit);
+		}
+	});
+
+	var ScrollAnimAgent = function(el, opts, attr) {
+		var agent = new ElAnimAgent(el, opts, attr);
+		mixMentor(this, agent);
+	};
+	ScrollAnimAgent.MENTOR_CLASS = ElAnimAgent;
+
+	mix(ScrollAnimAgent.prototype, {
+		getValue : function() {
+			return this.el[this.attr] | 0;
+		},
+		setValue : function(value) {
+			this.el[this.attr] = Math.round(value);
+		}
+	}, true);
+
+	var ColorAnimAgent = function(el, opts, attr) {
+		var agent = new ElAnimAgent(el, opts, attr);
+		mixMentor(this, agent);
+	};
+	ColorAnimAgent.MENTOR_CLASS = ElAnimAgent;
+
+	mix(ColorAnimAgent.prototype, {
+		parseColor : function(s){
+			var patterns = {
+				rgb         : /^rgb\(([0-9]+)\s*,\s*([0-9]+)\s*,\s*([0-9]+)\)$/i,
+				hex         : /^#?([0-9A-F]{2})([0-9A-F]{2})([0-9A-F]{2})$/i,
+				hex3        : /^#?([0-9A-F]{1})([0-9A-F]{1})([0-9A-F]{1})$/i
+			};
+
+			if (s.length == 3) { return s; }
+			
+			var c = patterns.hex.exec(s);
+			
+			if (c && c.length == 4) {
+				return [ parseInt(c[1], 16), parseInt(c[2], 16), parseInt(c[3], 16) ];
+			}
+		
+			c = patterns.rgb.exec(s);
+			if (c && c.length == 4) {
+				return [ parseInt(c[1], 10), parseInt(c[2], 10), parseInt(c[3], 10) ];
+			}
+		
+			c = patterns.hex3.exec(s);
+			if (c && c.length == 4) {
+				return [ parseInt(c[1] + c[1], 16), parseInt(c[2] + c[2], 16), parseInt(c[3] + c[3], 16) ];
+			}
+			
+			return [0, 0, 0];
+		},
+		init : function(){
+			var from, to, by, unit;
+			var parseColor = this.parseColor;
+
+			if(null != this.from){
+				from = this.from;			
+			}else{
+				from = this.getValue();
+			}
+
+			from = parseColor(from);
+
+			to = this.to || [0,0,0];
+			to = parseColor(to);
+
+			by = this.by ? parseColor(this.by) : 
+				map(to, function(d,i){
+					return d - from[i];
+				});
+
+			this.from = from;
+			this.to = to;
+			this.by = by;
+			this.unit = '';
+		},
+		getValue : function() {
+			var color = getCurrentStyle(this.el, this.attr);
+			return this.parseColor(color);
+		},
+		setValue : function(value) {
+			if(typeof value == "string") {
+				setStyle(this.el, this.attr, value);
+			} else {
+				setStyle(this.el, this.attr, "rgb("+value.join(",")+")");
+			}
+		},
+		action : function(per){
+			var me = this, value;
+			if (typeof this.end != "undefined" && per >= 1) {
+				value = this.end;
+			} else {
+				value = this.from.map(function(s, i){
+					return Math.max(Math.floor(s + me.by[i] * me.easing(per)),0);
+				});
+			}
+			this.setValue(value);
+		}
+	}, true);
+
+	var _agentPattern = { 
+		"color$" : ColorAnimAgent, 
+		"^scroll" : ScrollAnimAgent,
+		".*" : ElAnimAgent
+	};
+
+	function _patternFilter(patternTable, key){
+		for(var i in patternTable){
+			var pattern = new RegExp(i, "i");
+			if(pattern.test(key)){
+				return patternTable[i];
+			}
+		}	
+		return null;
+	};
+
+	var ElAnim = function(el, attrs, dur, easing) {
+		el = g(el);
+		if(!isElement(el)) {
+			throw new Error(['Animation','Initialize Error','Element Not Found!']);
+		}
+		dur = dur || ElAnim.DefaultEasing;
+		easing = typeof easing === 'function' ? easing : ElAnim.DefaultEasing;
+
+		var agents = [];
+		for(var attr in attrs){
+			var Agent = _patternFilter(_agentPattern, attr);
+			agent = new Agent(el, attrs[attr], attr);
+			if(!agent) continue;
+			agent.init();
+			agent.easing = agent.easing || easing;
+			agents.push(agent);
+		}
+
+		var anim = new Anim(function(per) {
+			agents.forEach(function(agent) {
+				agent.action(per);
+			});
+		}, dur);
+
+		mixMentor(this, anim); 
+	};
+
+	ElAnim.MENTOR_CLASS = Anim;
+	ElAnim.DefaultEasing = function(p) { return p;};
+	ElAnim.DefaultDur = 500;
+
+	QW.provide({
+		ElAnim: ElAnim,
+		ScrollAnim: ElAnim,
+		ColorAnim: ElAnim
+	});
+}());
